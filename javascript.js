59afc6216072df27f4f5b1dd90accb5a65c57627
Stefano Martin <see.martiinez@gmail.com>
2021-10-24 21:35:27 +0200

    ahora al selecionar tu personaje segun el orden sera jugador 1 o 2

--- javascript.js
+++ javascript.js
@@ -19,3 +19,87 @@ let j1 = new Player('Incineroar', 100, "https://projectpokemon.org/images/sprite
 let j2 = new Player('Pikachu', 100, "https://projectpokemon.org/images/sprites-models/homeimg/poke_capture_0025_001_mo_n_00000000_f_n.png") //personajes2
 let j3 = new Player('mewtwo', 100, "https://projectpokemon.org/images/sprites-models/homeimg/poke_capture_0150_000_uk_n_00000000_f_n.png")//personajes3
 let j4 = new Player('Greninja', 100, "https://projectpokemon.org/images/sprites-models/homeimg/poke_capture_0658_000_mf_n_00000000_f_n.png")//personaje4
+
+class Jugador {
+
+    constructor(player) {
+        this.personaje = player
+
+    }
+  
+}
+let jugador1 = new Jugador()
+console.log(jugador1) // jugador1.personaje = unfenid
+let jugador2 = new Jugador()
+console.log(jugador2) // jugador2.personaje = unfenid
+
+
+const seleccionarJugador = (player) => {
+
+
+    if (jugador1.personaje == undefined || jugador2.personaje == undefined) {
+
+
+        switch (player) {
+
+
+            case 'incineroar'://personaje1
+                if (jugador1.personaje == undefined) {
+
+                    jugador1.personaje = j1;
+                    console.log(jugador1.personaje)
+
+                } else if (jugador2.personaje == undefined && jugador1.personaje !== j1) {
+
+                    jugador2.personaje = j1;
+                   console.log(jugador2.personaje)
+                    
+                    escenarioLucha()
+                }
+                break
+            case 'pikachu'://personaje2
+                if (jugador1.personaje == undefined) {
+
+                    jugador1.personaje = j2;
+                    console.log(jugador1.personaje)
+
+                } else if (jugador2.personaje == undefined && jugador1.personaje !== j2) {
+                    jugador2.personaje = j2;
+                    console.log(jugador2.personaje)
+                    escenarioLucha()
+                }
+                break
+
+            case 'mewtwo'://personaje3
+                if (jugador1.personaje == undefined) {
+
+                    jugador1.personaje = j3;
+                    console.log(jugador1.personaje)
+
+                } else if (jugador2.personaje == undefined && jugador1.personaje !== j3) {
+                    
+                    jugador2.personaje = j3;
+                    console.log(jugador2.personaje)
+                    
+                    escenarioLucha()
+                }
+                break
+            case 'greninja'://personaje4
+                if (jugador1.personaje == undefined) {
+
+                    jugador1.personaje = j4;
+                    console.log(jugador1.personaje)
+
+                } else if (jugador2.personaje == undefined && jugador1.personaje !== j4) {
+
+                    jugador2.personaje = j4;
+                    console.log(jugador2.personaje)
+                    
+                    escenarioLucha()
+                }
+                break
+       
+            }
+           
+      } 
+}
